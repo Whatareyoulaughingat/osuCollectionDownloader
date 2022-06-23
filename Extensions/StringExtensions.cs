ec63f0883bc457bceb2e9f849d212236b9ed643a
Whatareyoulaughingat <nikosnaoumv1@gmail.com>
2022-06-24 00:38:17 +0300

    Extension method for replacing invalid path characters in a string + optimizations.

---
+++ Extensions/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace osuCollectionDownloader.Extensions;
+
+public static class StringExtensions
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static string ReplaceInvalidPathChars(this string source)
+    {
+        return new StringBuilder(source)
+            .Replace(":", "_")
+            .Replace("?", string.Empty)
+            .Replace("/", string.Empty)
+            .Replace("\"", string.Empty)
+            .Replace("<", string.Empty)
+            .Replace(">", string.Empty)
+            .Replace("|", string.Empty)
+            .Replace("*", string.Empty)
+            .Replace(".", string.Empty)
+            .ToString();
+    }
+}
