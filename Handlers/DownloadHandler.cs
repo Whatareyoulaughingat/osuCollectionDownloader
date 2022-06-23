131b1b6e67bdcc7dae4bf7eefac478e25eb45c2e
Whatareyoulaughingat <nikosnaoumv1@gmail.com>
2022-06-24 00:37:34 +0300

    Moved the code for downloading and generating collections to another class for code reuse.

---
+++ Handlers/DownloadHandler.cs
@@ -0,0 +1,145 @@
+﻿using CollectionManager.DataTypes;
+using CollectionManager.Enums;
+using CollectionManager.Modules.CollectionsManager;
+using CollectionManager.Modules.FileIO;
+using Flurl.Http;
+using osuCollectionDownloader.Extensions;
+using osuCollectionDownloader.Global;
+using osuCollectionDownloader.Models;
+using osuCollectionDownloader.ViewModels;
+using Serilog;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace osuCollectionDownloader.Handlers;
+
+public class DownloadHandler
+{
+    private BeatmapCollectionInfo currentCollectionInfo;
+
+    private BeatmapCollection currentBeatmapCollection;
+
+    private readonly OsuFileIo osuFileIo = new(new BeatmapExtension());
+
+    public async Task DownloadBeatmapsAsync()
+    {
+        if (Process.GetProcessesByName("osu!").Any())
+        {
+            Process.GetProcessesByName("osu!").First().Kill();
+            Log.Information("Closed osu! in order to import beatmaps.");
+        }
+
+        int totalBeatmapsDownloaded = 0;
+        long? nextPageCursor = 0;
+
+        currentCollectionInfo = await $"https://osucollector.com/api/collections/{MainWindowViewModel.Instance.CollectionId}".GetJsonAsync<BeatmapCollectionInfo>();
+
+        MainWindowViewModel.Instance.TitlebarName = $"osu! Collection Downloader - Importing: {currentCollectionInfo.Name}";
+
+        do
+        {
+            currentBeatmapCollection = await $"https://osucollector.com/api/collections/{MainWindowViewModel.Instance.CollectionId}/beatmapsv2?cursor={nextPageCursor}".GetJsonAsync<BeatmapCollection>();
+            nextPageCursor = currentBeatmapCollection.NextPageCursor;
+
+            foreach (Models.Beatmap beatmap in currentBeatmapCollection.Beatmaps)
+            {
+                string beatmapFileName = $"{beatmap.BeatmapsetId} {beatmap.Beatmapset.ArtistUnicode.ReplaceInvalidPathChars()} - {beatmap.Beatmapset.TitleUnicode.ReplaceInvalidPathChars()}.osz";
+                string beatmapInOsuDirectory = Path.GetFullPath(Path.GetFileNameWithoutExtension(beatmapFileName), Path.GetFullPath("Songs", MainWindowViewModel.Instance.OsuDirectory));
+                string beatmapInDownloadedBeatmapsDirectory;
+
+                if (Directory.Exists(beatmapInOsuDirectory))
+                {
+                    Log.Information($"Skipped {beatmap.Id} - {beatmap.Beatmapset.ArtistUnicode} - {beatmap.Beatmapset.TitleUnicode} because it's already imported.");
+                    continue;
+                }
+
+                try
+                {
+                    beatmapInDownloadedBeatmapsDirectory = await $"https://beatconnect.io/b/{beatmap.BeatmapsetId}".DownloadFileAsync(Paths.DownloadedBeatmapsDirectory, beatmapFileName);
+                }
+                catch (FlurlHttpException flurlHttpEx) when (flurlHttpEx.StatusCode == 404)
+                {
+                    beatmapInDownloadedBeatmapsDirectory = await $"https://kitsu.moe/api/d/{beatmap.BeatmapsetId}".DownloadFileAsync(Paths.DownloadedBeatmapsDirectory, beatmapFileName);
+                }
+                catch (Exception ex)
+                {
+                    Log.Error(ex, ex.Message);
+                    break;
+                }
+
+                ZipFile.ExtractToDirectory(beatmapInDownloadedBeatmapsDirectory, beatmapInOsuDirectory);
+                File.Delete(beatmapInDownloadedBeatmapsDirectory);
+
+                totalBeatmapsDownloaded++;
+                Log.Information($"Imported {beatmap.Id} - {beatmap.Beatmapset.ArtistUnicode} - {beatmap.Beatmapset.TitleUnicode}");
+            }
+
+        } while (currentBeatmapCollection.HasMore);
+
+        Log.Information($"Finished downloading {totalBeatmapsDownloaded} beatmaps.");
+        MainWindowViewModel.Instance.TitlebarName = "osu! Collection Downloader";
+    }
+
+    public void GenerateCollection()
+    {
+        Collections GenerateCollectionDb(Beatmaps maps, string collectionName)
+        {
+            Collections osuCollections = new();
+            Collection currentCollection = new(osuFileIo.LoadedMaps) { Name = collectionName };
+
+            foreach (CollectionManager.DataTypes.Beatmap map in maps)
+            {
+                currentCollection.AddBeatmap(map);
+            }
+
+            osuCollections.Add(currentCollection);
+            return osuCollections;
+        }
+
+        Beatmaps collectionDbBeatmaps = new();
+
+        foreach (Models.Beatmap beatmap in currentBeatmapCollection.Beatmaps)
+        {
+            BeatmapExtension beatmapData = new()
+            {
+                MapSetId = beatmap.BeatmapsetId,
+                MapId = beatmap.Id,
+                CircleSize = beatmap.Cs,
+                OverallDifficulty = beatmap.Accuracy,
+                ApproachRate = beatmap.Ar,
+                HpDrainRate = beatmap.Drain,
+                Circles = beatmap.CountCircles,
+                Sliders = beatmap.CountSliders,
+                Spinners = beatmap.CountSpinners,
+                MainBpm = beatmap.Bpm,
+                DiffName = beatmap.Version,
+                Md5 = beatmap.Checksum,
+                ArtistRoman = beatmap.Beatmapset.Artist,
+                TitleRoman = beatmap.Beatmapset.Title,
+                ArtistUnicode = beatmap.Beatmapset.ArtistUnicode,
+                TitleUnicode = beatmap.Beatmapset.TitleUnicode,
+                Creator = beatmap.Beatmapset.Creator,
+                Source = beatmap.Beatmapset.Source,
+                Tags = beatmap.Beatmapset.Tags,
+                EditDate = beatmap.LastUpdated,
+                PlayMode = (PlayMode)beatmap.ModeInt,
+                DataDownloaded = true,
+            };
+
+            beatmapData.ModPpStars.Add(beatmapData.PlayMode, new() { { 0, Math.Round(beatmap.DifficultyRating, 2) } });
+            collectionDbBeatmaps.Add(beatmapData);
+        }
+
+        CollectionsManager osuCollectionsManager = new(osuFileIo.OsuDatabase.LoadedMaps.Beatmaps);
+        osuCollectionsManager.EditCollection(CollectionEditArgs.AddCollections(GenerateCollectionDb(collectionDbBeatmaps, currentCollectionInfo.Name)));
+
+        osuFileIo.CollectionLoader.SaveOsdbCollection(osuCollectionsManager.LoadedCollections, Path.GetFullPath($"{currentCollectionInfo.Name.ReplaceInvalidPathChars()}.osdb", MainWindowViewModel.Instance.CollectionDbOutput));
+
+        Log.Information($"Generated an .osdb file in {MainWindowViewModel.Instance.CollectionDbOutput}");
+        MainWindowViewModel.Instance.TitlebarName = "osu! Collection Downloader";
+    }
+}
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,28 +1,21 @@
-﻿using CollectionManager.DataTypes;
-using CollectionManager.Enums;
-using CollectionManager.Modules.CollectionsManager;
-using CollectionManager.Modules.FileIO;
-using Flurl.Http;
-using Ookii.Dialogs.Wpf;
-using osuCollectionDownloader.Global;
-using osuCollectionDownloader.Models;
+﻿using Ookii.Dialogs.Wpf;
+using osuCollectionDownloader.Handlers;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 using Serilog;
+using Splat;
 using System;
-using System.Diagnostics;
-using System.IO;
-using System.IO.Compression;
 using System.Linq;
 using System.Reactive;
 using System.Reactive.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace osuCollectionDownloader.ViewModels;
 
 public class MainWindowViewModel : ReactiveObject
 {
+    public static MainWindowViewModel Instance { get; private set; }
+
     [Reactive]
     public string TitlebarName { get; set; } = "osu! Collection Downloader";
 
@@ -46,10 +39,15 @@ public class MainWindowViewModel : ReactiveObject
 
     public ReactiveCommand<Unit, Unit> StartCollectionDbDownload { get; }
 
-    private readonly OsuFileIo osuFileIo = new(new BeatmapExtension());
+    private DownloadHandler Downloader { get; }
 
     public MainWindowViewModel()
     {
+        Instance = this;
+
+        Locator.CurrentMutable.RegisterConstant(new DownloadHandler());
+        Downloader = Locator.Current.GetService<DownloadHandler>();
+
         IObservable<bool> canStartDownload = this.WhenAnyValue(x => x.OsuDirectory, x => x.CollectionId,
                                                                selector: (osuDirectory, collectionId) => !string.IsNullOrWhiteSpace(osuDirectory) &&
                                                                                                          !string.IsNullOrWhiteSpace(collectionId) &&
@@ -88,169 +86,12 @@ public class MainWindowViewModel : ReactiveObject
     }
 
     private async Task StartDownloadAsyncImpl()
-    {
-        if (Process.GetProcessesByName("osu!").Any())
-        {
-            Process.GetProcessesByName("osu!").First().Kill();
-            Log.Information("Closing osu! in order to import beatmaps.");
-        }
-
-        int totalBeatmapCount = 0;
-        int beatmapPerPageCount = 0;
-        long? cursor = 0;
-
-        BeatmapCollection beatmapCollection;
-
-        CollectionInfo collectionInfo = await $"https://osucollector.com/api/collections/{CollectionId}".GetJsonAsync<CollectionInfo>();
-
-        do
-        {
-            beatmapCollection = await $"https://osucollector.com/api/collections/{CollectionId}/beatmapsv2?cursor={cursor}".GetJsonAsync<BeatmapCollection>();
-
-            cursor = beatmapCollection.NextPageCursor;
-            TitlebarName = $"osu! Collection Downloader - Downloading: {collectionInfo.Name}";
-
-            foreach (Models.Beatmap beatmap in beatmapCollection.Beatmaps)
-            {
-                if (beatmapPerPageCount >= 50)
-                {
-                    beatmapPerPageCount = 0;
-                    continue;
-                }
-
-                try
-                {
-                    string beatmapFileName = $"{beatmap.BeatmapsetId} {ReplaceInvalidPathCharacters(beatmap.Beatmapset.ArtistUnicode)} - {ReplaceInvalidPathCharacters(beatmap.Beatmapset.TitleUnicode)}.osz";
-                    string beatmapInOsuDirectory = Path.GetFullPath(Path.GetFileNameWithoutExtension(beatmapFileName), Path.GetFullPath("Songs", OsuDirectory));
-
-                    if (!Directory.Exists(beatmapInOsuDirectory))
-                    {
-                        string beatmapInDownloadedBeatmapsDirectory = await $"https://beatconnect.io/b/{beatmap.BeatmapsetId}".DownloadFileAsync(Paths.DownloadedBeatmapsDirectory, beatmapFileName);
-
-                        ZipFile.ExtractToDirectory(beatmapInDownloadedBeatmapsDirectory, beatmapInOsuDirectory);
-                        File.Delete(beatmapInDownloadedBeatmapsDirectory);
-
-                        Log.Information($"Imported {beatmap.Id} - {beatmap.Beatmapset.ArtistUnicode} - {beatmap.Beatmapset.TitleUnicode}");
-                    }
-                    else
-                    {
-                        Log.Information($"Skipping {beatmap.Id} - {beatmap.Beatmapset.ArtistUnicode} - {beatmap.Beatmapset.TitleUnicode} because it's already imported.");
-                    }
-                }
-                catch
-                {
-                    continue;
-                }
-                finally
-                {
-                    beatmapPerPageCount++;
-                    totalBeatmapCount++;
-                }
-            }
-
-        } while (beatmapCollection.HasMore);
-
-        Log.Information($"Finished downloading {totalBeatmapCount} beatmaps.");
-        TitlebarName = "osu! Collection Downloader";
-    }
+        => await Downloader.DownloadBeatmapsAsync().ConfigureAwait(false);
 
     private async Task StartCollectionDbDownloadAsyncImpl()
     {
-        if (Process.GetProcessesByName("osu!").Any())
-        {
-            Process.GetProcessesByName("osu!").First().Kill();
-            Log.Information("Closing osu! in order to import beatmaps.");
-        }
-
-        int beatmapPerPageCount = 0;
-        long? cursor = 0;
-
-        BeatmapCollection beatmapCollection;
-        Beatmaps collectionDbBeatmaps = new();
-
-        CollectionInfo collectionInfo = await $"https://osucollector.com/api/collections/{CollectionId}".GetJsonAsync<CollectionInfo>();
-
-        do
-        {
-            beatmapCollection = await $"https://osucollector.com/api/collections/{CollectionId}/beatmapsv2?cursor={cursor}".GetJsonAsync<BeatmapCollection>();
-
-            cursor = beatmapCollection.NextPageCursor;
-            TitlebarName = $"osu!CollectorGrabber - Downloading: {collectionInfo.Name}";
-
-            foreach (Models.Beatmap beatmap in beatmapCollection.Beatmaps)
-            {
-                if (beatmapPerPageCount >= 50)
-                {
-                    beatmapPerPageCount = 0;
-                    continue;
-                }
-
-                try
-                {
-                    string beatmapFileName = $"{beatmap.BeatmapsetId} {ReplaceInvalidPathCharacters(beatmap.Beatmapset.ArtistUnicode)} - {ReplaceInvalidPathCharacters(beatmap.Beatmapset.TitleUnicode)}.osz";
-                    string beatmapInOsuDirectory = Path.GetFullPath(Path.GetFileNameWithoutExtension(beatmapFileName), Path.GetFullPath("Songs", OsuDirectory));
-
-                    if (!Directory.Exists(beatmapInOsuDirectory))
-                    {
-                        string beatmapInDownloadedBeatmapsDirectory = await $"https://beatconnect.io/b/{beatmap.BeatmapsetId}".DownloadFileAsync(Paths.DownloadedBeatmapsDirectory, beatmapFileName);
-
-                        ZipFile.ExtractToDirectory(beatmapInDownloadedBeatmapsDirectory, beatmapInOsuDirectory);
-                        File.Delete(beatmapInDownloadedBeatmapsDirectory);
-
-                        Log.Information($"Imported {beatmap.Id} - {beatmap.Beatmapset.ArtistUnicode} - {beatmap.Beatmapset.TitleUnicode}");
-                    }
-                    else
-                    {
-                        Log.Information($"Skipping {beatmap.Id} - {beatmap.Beatmapset.ArtistUnicode} - {beatmap.Beatmapset.TitleUnicode} because it's already imported.");
-                    }
-                }
-                catch
-                {
-                    continue;
-                }
-                finally
-                {
-                    BeatmapExtension beatmapData = new()
-                    {
-                        MapSetId = beatmap.BeatmapsetId,
-                        MapId = beatmap.Id,
-                        CircleSize = (float)beatmap.Cs,
-                        OverallDifficulty = (float)beatmap.Accuracy,
-                        ApproachRate = (float)beatmap.Ar,
-                        HpDrainRate = (float)beatmap.Drain,
-                        Circles = (short)beatmap.CountCircles,
-                        Sliders = (short)beatmap.CountSliders,
-                        Spinners = (short)beatmap.CountSpinners,
-                        MainBpm = beatmap.Bpm,
-                        DiffName = beatmap.Version,
-                        Md5 = beatmap.Checksum,
-                        ArtistRoman = beatmap.Beatmapset.Artist,
-                        TitleRoman = beatmap.Beatmapset.Title,
-                        ArtistUnicode = beatmap.Beatmapset.ArtistUnicode,
-                        TitleUnicode = beatmap.Beatmapset.TitleUnicode,
-                        Creator = beatmap.Beatmapset.Creator,
-                        Source = beatmap.Beatmapset.Source,
-                        Tags = beatmap.Beatmapset.Tags,
-                        EditDate = beatmap.LastUpdated,
-                        PlayMode = (PlayMode)beatmap.ModeInt,
-                        DataDownloaded = true,
-                    };
-
-                    beatmapData.ModPpStars.Add(beatmapData.PlayMode, new() { { 0, Math.Round(beatmap.DifficultyRating, 2) } });
-                    collectionDbBeatmaps.Add(beatmapData);
-
-                    beatmapPerPageCount++;
-                }
-            }
-
-        } while (beatmapCollection.HasMore);
-
-        CollectionsManager collectionsManager = new(osuFileIo.OsuDatabase.LoadedMaps.Beatmaps);
-        collectionsManager.EditCollection(CollectionEditArgs.AddCollections(GenerateCollection(collectionDbBeatmaps, collectionInfo.Name)));
-        osuFileIo.CollectionLoader.SaveOsdbCollection(collectionsManager.LoadedCollections, Path.GetFullPath($"{ReplaceInvalidPathCharacters(collectionInfo.Name)}.osdb", CollectionDbOutput));
-
-        Log.Information($"Finished downloading {collectionDbBeatmaps.Count} beatmaps & generated an .osdb file in {CollectionDbOutput}");
-        TitlebarName = "osu!CollectorGrabber";
+        await Downloader.DownloadBeatmapsAsync().ConfigureAwait(false);
+        Downloader.GenerateCollection();
     }
 
     private string OpenOsuDirectoryDialogImpl()
@@ -279,7 +120,7 @@ public class MainWindowViewModel : ReactiveObject
         VistaFolderBrowserDialog collectionDbOutputFolderDialog = new()
         {
             RootFolder = Environment.SpecialFolder.ApplicationData,
-            Description = "Select a folder to save your collection.db file.",
+            Description = "Select a folder to save your collection.osdb file.",
             UseDescriptionForTitle = true,
             ShowNewFolderButton = true,
             Multiselect = false,
@@ -293,34 +134,5 @@ public class MainWindowViewModel : ReactiveObject
         }
 
         return collectionDbOutputFolderDialog.SelectedPath;
-    }
-
-    private Collections GenerateCollection(Beatmaps maps, string collectionName)
-    {
-        Collections collections = new();
-        Collection collection = new(osuFileIo.LoadedMaps) { Name = collectionName };
-
-        foreach (CollectionManager.DataTypes.Beatmap map in maps)
-        {
-            collection.AddBeatmap(map);
-        }
-
-        collections.Add(collection);
-        return collections;
-    }
-
-    private static string ReplaceInvalidPathCharacters(string source)
-    {
-        return new StringBuilder(source)
-            .Replace(":", "_")
-            .Replace("?", string.Empty)
-            .Replace("/", string.Empty)
-            .Replace("\"", string.Empty)
-            .Replace("<", string.Empty)
-            .Replace(">", string.Empty)
-            .Replace("|", string.Empty)
-            .Replace("*", string.Empty)
-            .Replace(".", string.Empty)
-            .ToString();
     }
 }
